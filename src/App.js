fa7c9500b49173331480fac0fd3915e00cd499dd
Yash Nagpaul <yash.nagpaul@gmail.com>
2021-01-02 16:16:15 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = "O41WyMvXUcvMbwzA3M8QxKVBAO60B31N";
+const trendingUrl = `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`;
+// const searchUrl = (q) =>
+//   `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${q}`;
+
+class App extends React.Component {
+  state = {
+    gifs: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    axios
+      .get(trendingUrl)
+      .then((response) =>
+        this.setState({ loading: false, gifs: response.data.data })
+      );
+  }
+  searchGifs = (event) => {
+    event.preventDefault();
+    const q = event.target.q.value;
+    event.target.q.value = "";
+
+    axios
+      .get(
+        `https://api.giphy.com/v1/gifs/search?q=${q}&api_key=O41WyMvXUcvMbwzA3M8QxKVBAO60B31N`
+      )
+      .then((response) => {
+        return this.setState({ loading: false, gifs: response.data.data });
+      });
+  };
+
+  render() {
+    const { loading, gifs } = this.state;
+
+    const gifsList = gifs.map((gif) => {
+      return (
+        <div key={gif.id}>
+          <img
+            src={gif.images.downsized_still.url}
+            alt=""
+            style={{ margin: "10px", width: "200px" }}
+          />
+        </div>
+      );
+    });
+
+    return (
+      <>
+        <h1 style={{ textAlign: "center" }}>Gifs</h1>
+        <form onSubmit={this.searchGifs} style={{ textAlign: "center" }}>
+          <input type="text" name="q" />
+          <button>Search</button>
+        </form>
+        {loading ? (
+          <div
+            style={{
+              textAlign: "center",
+            }}
+          >
+            <h1>Loading Gifs...</h1>
+          </div>
+        ) : (
+          <div style={{ display: "flex", flexWrap: "wrap" }}>{gifsList}</div>
+        )}
+      </>
+    );
+  }
 }
 
 export default App;
